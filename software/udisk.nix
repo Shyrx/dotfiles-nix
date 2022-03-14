e3c84b1afc41d162059196269474dcf9a6f0c742
Antoine Solé <antoine.sole@epita.fr>
2022-03-14 18:04:34 +0100

    software: udiskie: add comments

--- software/udisk.nix
+++ software/udisk.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }:
 
+# Packages udiskie and ntfs3g must be added system wide otherwise they do not
+# enough permissions to mount/unmount disks.
+
 {
   services.udiskie = {
     enable = true;
-    automount = true;
-    notify = true;
   };
 }
