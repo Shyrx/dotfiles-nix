59653d8c5b1f900280316f5e964e00cb1803f035
Antoine Solé <antoine.sole@epita.fr>
2022-03-19 18:24:06 +0100

    system: network: default: comments

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,36 +1,40 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Trying networkd
+  # Enable and
   systemd.network.enable = true;
+  useNetworkd = true;
 
-  # systemd-resolved
+  # systemd-resolved, a systemd DNS resolver daemon
+  # Required to obtain DNS adresses from DHCP servers
   services.resolved = {
     enable = true;
-    extraConfig = ''
-    '';
   };
 
-
+  # Need to specify that dhcpcd is disable
+  # to use the of internal to networkd
   dhcpcd.enable = false;
-  useNetworkd = true;
 
   networking = {
     wireless.iwd = {
       enable = true;
+
       settings = {
         General = {
           EnableNetworkConfiguration=true; # Enable integrated dhcp client
-          DisableANQP=false;
-          # SystemdEncrypt=""; // TODO search on this
+          DisableANQP=false; # Use if Kernel version > 5.3
+          # SystemdEncrypt=""; # TODO encrypt password for known network, experimental features, need to set-up by hand
         };
         Network = {
           EnableIPv6=true;
         };
         Settings = {
-          AutoConnect=true;
+          AutoConnect=true; # Auto connect to known network
         };
       };
     };
   };
+
+
+  # FIXME Export iwd env var
 }
