bcdcd2730adb089a3107a59f2958c1265c677b83
Antoine Solé <antoine.sole@epita.fr>
2022-09-22 23:14:00 +0200

    fix(nvidia): fix dpi problems

--- hosts/nixos-lp-omen/default.nix
+++ hosts/nixos-lp-omen/default.nix
@@ -1,12 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix # Include the results of the hardware scan.
-      ./nvidia.nix
-      ../common
-    ];
+  imports = [
+    ./hardware-configuration.nix # Include the results of the hardware scan.
+    ./nvidia.nix
+    ../common
+  ];
 
   networking.hostName = "nixos-lp-omen";
 }
