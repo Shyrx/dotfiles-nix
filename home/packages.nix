9f3bdf12826a393588bbdae65dce26c78ddf878c
Antoine Solé <antoine.sole@epita.fr>
2022-09-23 15:18:50 +0200

    fix(packages): add packages in accordance with doom doctor

--- home/packages.nix
+++ home/packages.nix
@@ -1,84 +1,99 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
 
-    nixpkgs.config.allowUnfree = true;
-    home.packages = with pkgs; [
-      # Software
-      discord
-      firefox
-      flameshot # screenshot
-      gimp
-      libreoffice
-      morgen
-      slack
-      spotify # Music
-      steam
-      teams
-      thunderbird
-      vlc
-      xfce.thunar
-      xfce.thunar-archive-plugin
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    # Software
+    discord
+    firefox
+    flameshot # screenshot
+    gimp
+    libreoffice
+    morgen
+    slack
+    spotify # Music
+    steam
+    teams
+    thunderbird
+    vlc
+    xfce.thunar
+    xfce.thunar-archive-plugin
 
-      # Session
-      # dunst # Notifications daemon
-      # picom # Compositing manager for X servers
-      alacritty # tty
-      betterlockscreen
-      feh # background image
-      i3status-rust # bar
-      i3wsr # rename workspace
-      numlockx
-      rofi # Another dmenu
-      zsh # Terminal
+    # Session
+    # dunst # Notifications daemon
+    # picom # Compositing manager for X servers
+    alacritty # tty
+    betterlockscreen
+    feh # background image
+    i3status-rust # bar
+    i3wsr # rename workspace
+    numlockx
+    rofi # Another dmenu
+    zsh # Terminal
 
-      # Command line tools
-      arandr # window managment
-      cmake
-      curl
-      direnv
-      docker
-      evince # pdf
-      exa # better ls
-      fd # faster find, used for doom
-      file
-      gcc
-      gdb
-      gnumake
-      gnutar
-      htop
-      pandoc # Convert documents
-      poetry # Venv in better
-      qemu # Machine emulator and virtualizer
-      ripgrep # Better grep, used for doom
-      tree
-      unrar
-      unzip
-      virtualenv
-      zip
+    # Command line tools
+    arandr # window managment
+    cmake
+    curl
+    direnv
+    docker
+    evince # pdf
+    exa # better ls
+    fd # faster find, used for doom
+    file
+    gcc
+    gdb
+    gnumake
+    gnutar
+    htop
+    pandoc # Convert documents
+    poetry # Venv in better
+    qemu # Machine emulator and virtualizer
+    ripgrep # Better grep, used for doom
+    tree
+    unrar
+    unzip
+    virtualenv
+    zip
 
-      # Utils
-      bear
-      binutils
-      cargo
-      ccls
-      clang-tools
-      editorconfig-core-c
-      font-awesome
-      glslang # For C lsp
-      gtk3
-      man-pages
-      man-pages-posix
-      nixfmt
-      pipenv
-      playerctl # Utility for controlling media players
-      pre-commit
-      python310
-      rustc
-      shellcheck # For better shell coding experience
-      python-language-server
+    # Utils
+    font-awesome
+    gtk3
+    playerctl # Utility for controlling media players
+    pre-commit
+    maim # For org
+    xclip # For org
 
-      gnupg
-      pinentry-curses
+    binutils
+    clang-tools
+    bear
+
+    ccls
+    editorconfig-core-c
+    glslang # For C lsp
+
+    man-pages
+    man-pages-posix
+
+    nixfmt
+
+    shellcheck # For better shell coding experience
+    shfmt
+
+    rustc
+    cargo
+    rust-analyzer
+
+    python310
+    python310Packages.pyflakes
+    python310Packages.isort
+    python310Packages.pytest
+    python-language-server
+    pipenv
+    black
+
+    gnupg
+    pinentry-curses
   ];
 }
