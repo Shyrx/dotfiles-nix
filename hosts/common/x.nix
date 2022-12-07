a59d6a08273b758c3e82abed7ed4add58c135a29
Antoine Solé <antoine.sole@epita.fr>
2022-12-07 22:18:32 +0100

    fix(hosts: common: x): remove useless comment

--- hosts/common/x.nix
+++ hosts/common/x.nix
@@ -8,11 +8,6 @@
     windowManager.i3 = {
       package = pkgs.i3-gaps;
       enable = true;
-      # extraPackages = with pkgs; [
-      #   dmenu
-      #   i3status
-      #   i3lock
-      # ];
     };
     displayManager = {
       defaultSession = "none+i3";
