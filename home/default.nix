1ac7e6b8bac5f5a654344443d7af1031dc6a9ba5
Antoine Solé <antoine.sole@epita.fr>
2022-12-07 22:17:06 +0100

    feat(home-manager): move base config to a specific file

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
   # Imports configurations from other files/directories
   imports = [
     ./packages.nix
@@ -11,25 +8,6 @@
     ./software
     ./tools
   ];
-
-
-  home = {
-    username = "shyrx";
-    homeDirectory = "/home/shyrx";
-
-    sessionVariables = {
-      EDITOR = "emacs";
-      BROWSER = "firefox";
-      # _JAVA_AWT_WM_NONREPARENTING = 1;
-      # DRI_PRIME = 1;
-      # __NV_PRIME_RENDER_OFFLOAD = 1;
-      # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      # PROTON_LOG = 1;
-      # PROTON_USE_WINED3D = 1;
-    };
-
-    stateVersion = "22.05";
-  };
 
   services = {
     blueman-applet.enable = true;
---
+++ hosts/common/home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  home-manager.users.shyrx = {
+    home = rec {
+      # Home Manager needs a bit of information about you and the
+      # paths it should manage.
+      username = "shyrx";
+      homeDirectory = "/home/${username}";
+
+      enableNixpkgsReleaseCheck = true;
+
+      sessionVariables = {
+        EDITOR = "emacs";
+        BROWSER = "firefox";
+      };
+
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "22.05";
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager = { enable = true; };
+  };
+}
