e021cebe4e71d613269ec68c9cb2d77b90d152cd
Antoine Solé <antoine.sole@epita.fr>
2023-01-02 17:50:03 +0100

    fix(home: alacritty): moved for module architecture

--- home/session/default.nix
+++ home/session/default.nix
@@ -1,13 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    ./alacritty.nix
-    ./bar.nix
-    ./gtk.nix
-    ./i3.nix
-    ./locker.nix
-    ./picom.nix
-    ./rofi
-  ];
+  imports = [ ./bar.nix ./gtk.nix ./i3.nix ./locker.nix ./picom.nix ./rofi ];
 }
